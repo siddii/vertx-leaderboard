b112366e81757fa3afe8e504b916c3c79b4e0d83
Siddique Hameed <siddique_hameed@mastercard.com>
2014-10-13 16:59:03 -0500

    dummy data

---
+++ data.js
@@ -0,0 +1,19 @@
+//Fill in some dummy data
+var data = {
+    "jQuery": {
+        "upVote": 0,
+        "downVote": 0
+    },
+    "AngularJS": {
+        "upVote": 0,
+        "downVote": 0
+    },
+    "NodeJS": {
+        "upVote": 0,
+        "downVote": 0
+    },
+    "Underscore": {
+        "upVote": 0,
+        "downVote": 0
+    }
+}
--- server.js
+++ server.js
@@ -5,16 +5,30 @@ var vertx = require('vertx');
 var server = vertx.createHttpServer();
 var console = require('vertx/console');
 
+//load some dummy data
+load('data.js');
+
 // Serve the static resources
 server.requestHandler(function (req) {
     var uri = req.uri();
     if (uri == "/") req.response.sendFile("index.html");
+
+    if (uri == '/data') {
+        req.response.end(JSON.stringify(data));
+    }
+
     if (uri && (uri.indexOf('js') !== -1 || uri.indexOf('.css') !== -1)) {
         req.response.sendFile(uri.substring(1));
     }
 });
 
+
 var sockJSServer = vertx.createSockJSServer(server);
 sockJSServer.bridge({prefix: "/leaderboard"}, [{}], [{}]);
+
+sockJSServer.on('socket-created', function (msg) {
+    console.log('##### Socket Created');
+    return true;
+});
 
 server.listen(9090);
