dcb7503095f7e8a4bd446ab25502a9be6b6585b1
Siddique Hameed <siddique_hameed@mastercard.com>
2014-10-14 08:46:07 -0500

    Up & down voting

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ angular.module('Leaderboard', []).controller("LeaderboardController", ['$scope',
         publish(frameworks);
     };
 
+    $scope.upVote = function (framework) {
+        var frameworks = angular.copy($scope.frameworks);
+        for(var i=0; i < frameworks.length; i++) {
+            if (frameworks[i].name === framework.name) {
+                frameworks[i].upVote++;
+                break;
+            }
+        }
+        publish(frameworks);
+    };
+
+    $scope.downVote = function (framework) {
+        var frameworks = angular.copy($scope.frameworks);
+        for(var i=0; i < frameworks.length; i++) {
+            if (frameworks[i].name === framework.name) {
+                frameworks[i].downVote++;
+                break;
+            }
+        }
+        publish(frameworks);
+    };
+
     initConn(function (response) {
         $scope.frameworks = response.text;
         $scope.$apply();
